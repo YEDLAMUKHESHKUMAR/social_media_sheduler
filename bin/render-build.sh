96709734d4e97e834cf6df80b9a4f2d4d862f209
YEDLAMUKHESHKUMAR <mukheshkumaryedla@gmail.com>
2025-07-18 17:33:25 +0530

    second commit - 2

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+# Skip db:migrate during build - it will run when the service starts
